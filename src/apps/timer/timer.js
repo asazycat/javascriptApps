bf6f2b9de4ea40d05075f02e6cf578a9c7c34c66
Asad Jawaid <asazycat@gmail.com>
2025-08-24 19:48:23 +0100

    clock set responsive for small screen

--- src/apps/timer/timer.js
+++ src/apps/timer/timer.js
@@ -2,7 +2,7 @@
 
 
 export default (function () {
-    let minute = 0, second = 0, deci_second = 0, s = 0, m =0 , h = 0, date = 0, intervalId, stopClock;
+    let minute = 0, second = 0, deci_second = 0, s = 0, m =0 , h = 0, date = 0, intervalId;
     const buttons = document.querySelector('#buttons');
     const start = document.querySelector('#start');
     const stop = document.querySelector('#stop');
@@ -12,11 +12,11 @@ export default (function () {
     const minutes = document.querySelector('#minutes')
     const timeElasped = document.querySelector('#timeElasped')
     const clockContext = document.querySelector('#canvasClock')
+    const clockDiv = document.querySelector(`.clock`)
     const clock = clockContext.getContext('2d')
-    
   
     window.onload = function () {
-        stopClock = setInterval(() => {
+        setInterval(() => {
                  date = new Date()
            
             s = date.getSeconds() 
@@ -25,46 +25,47 @@ export default (function () {
        
         clock.reset()
         clock.beginPath();
-        clock.moveTo(500, 250);
-         clock.arc(250, 250, 250, 0, 2 * Math.PI)
+        clock.moveTo(300, 150);
+         clock.arc(150, 150, 150, 0, 2 * Math.PI)
         clock.closePath()
         clock.stroke()
         
-            for (let i = 0; i < 360; i += 30) {
+            for (let i = 30; i <= 360; i += 30) {
                 clock.beginPath();
-                clock.moveTo(250, 250);
+                clock.moveTo(300, 150);
+                clock.fill()
                 clock.font = "25px serif";
-                clock.fillText(i/30 ,Math.cos((i - 90)* (Math.PI / 180)) * 200 + 250, Math.sin((i - 90) * (Math.PI / 180)) * 200 + 250, )
+                clock.fillText(i/30 ,Math.cos((i - 90)* (Math.PI / 180)) * 100 + 140 , Math.sin((i - 90) * (Math.PI / 180)) * 100 + 150 )
                 clock.closePath()
                 clock.stroke()
             }
 
         clock.beginPath()
-        clock.moveTo(250, 250) 
+        clock.moveTo(150, 150) 
         clock.strokeStyle = 'blue'
-        clock.arc(250, 250, 200, (Math.PI / 30) * s - Math.PI/2, (Math.PI / 30) * s - Math.PI/2)
+        clock.arc(150, 150, 130, (Math.PI / 30) * s - Math.PI/2, (Math.PI / 30) * s - Math.PI/2)
         clock.stroke()
             
 
         clock.beginPath()
-        clock.moveTo(250, 250)
+        clock.moveTo(150, 150)
         clock.strokeStyle = 'red'
-        clock.arc(250, 250, 150, (Math.PI / 30) * m - Math.PI/2, (Math.PI / 30) * m - Math.PI/2)
+        clock.arc(150, 150, 130, (Math.PI / 30) * m - Math.PI/2, (Math.PI / 30) * m - Math.PI/2)
         clock.stroke()
             
         clock.beginPath()
-        clock.moveTo(250, 250)
+        clock.moveTo(150, 150)
         clock.strokeStyle = 'green'
-        clock.arc(250, 250, 100, (Math.PI / 6) * (h + m/60) - Math.PI/2, (Math.PI / 6) * (h + m/60) - Math.PI/2)
+        clock.arc(150, 150, 100, (Math.PI / 6) * (h + m/60) - Math.PI/2, (Math.PI / 6) * (h + m/60) - Math.PI/2)
         clock.stroke()
        
         if (date.getSeconds() === 0) {
                 
               
             clock.beginPath()
-            clock.moveTo(250, 250)
+            clock.moveTo(150, 150)
             clock.strokeStyle = 'red'
-            clock.arc(250, 250, 150, (Math.PI / 30) * m - Math.PI/2, (Math.PI / 30) * m - Math.PI/2)
+            clock.arc(150, 150, 100, (Math.PI / 30) * m - Math.PI/2, (Math.PI / 30) * m - Math.PI/2)
             clock.stroke()
             }
             
@@ -72,9 +73,9 @@ export default (function () {
                 
                
             clock.beginPath()
-            clock.moveTo(250, 250)
+            clock.moveTo(150, 150)
             clock.strokeStyle = 'green'
-            clock.arc(250, 250, 100, (Math.PI / 6) * (h + m/60) - Math.PI/2 , (Math.PI / 6) * (h + m/60) - Math.PI/2)
+            clock.arc(150, 150, 50, (Math.PI / 6) * (h + m/60) - Math.PI/2 , (Math.PI / 6) * (h + m/60) - Math.PI/2)
             clock.stroke()
          }
     }, 1000)
@@ -115,7 +116,7 @@ export default (function () {
             stop.style.display = 'none'
             start.style.display = 'block'
         } else if (e.target === reset) {
-             clearInterval(stopClock)
+             
             clearInterval(intervalId)
             timeElasped.innerText = `${minute > 1 ? `${minute} minutes` : `${minute} minute`}  ${second > 1 ? `${second} seconds` : `${second} second`}  ${deci_second > 1 ? `${deci_second} deciseconds` : `${deci_second} decisecond`} `
             minute = 0, second = 0, deci_second = 0
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import fs from 'fs'
 const rootIndex = resolve(__dirname, 'index.html')
 const appsDir = resolve(__dirname, 'src/apps')
 
-// collect all app index.html files
 const appsInput = fs.readdirSync(appsDir)
   .filter(name => fs.existsSync(resolve(appsDir, name, 'index.html')))
   .reduce((entries, name) => {
@@ -13,7 +12,7 @@ const appsInput = fs.readdirSync(appsDir)
     return entries
   }, {})
 
-// include the root index.html as "main"
+
 const input = {
   main: rootIndex,
   ...appsInput
