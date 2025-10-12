7484100d42d0265137be7a27c0dc210aa1b393e9
unknown <asazycat@gmail.com>
2025-10-12 12:28:13 +0100

    changes

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const app = document.getElementById('app');
 console.log(app)
 app.addEventListener('click', function (e) {
     if (e.target.className.includes('item')) {
-         e.target.style.display === 'block' ? e.target.style.display = 'none' : e.target.style.display = 'block'
+        e.target.lastElementChild.style.display === 'block' ? e.target.lastElementChild.style.display = 'none' : e.target.lastElementChild.style.display = 'block'
     }
     
 })
