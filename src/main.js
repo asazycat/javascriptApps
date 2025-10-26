d2815400d3e18abb126794e90f2700b71c7f886b
unknown <asazycat@gmail.com>
2025-10-26 06:32:29 +0000

    some changes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const APP = document.getElementById('app');
+const APP_LIST = document.getElementById('apps_list');
 const items = document.getElementsByClassName('items')
 
 let elementDes = null, elementTitle = null;
@@ -11,38 +12,34 @@ APP.addEventListener('click', function(e) {
             elementTitle.className = 'itemTitle'
             elementDes.className = 'itemDescription'
         }
-        e.target.parentElement.style = 'opacity: 0.2'
+        e.target.parentElement.style = 'background-color:#f4f75433'
         e.target.className = 'itemTitleOff'
         e.target.nextElementSibling.className = 'itemDescriptionOn'
         elementTitle = e.target
         elementDes =  e.target.nextElementSibling
 
         if(e.target.textContent === 'JavaScript Applications and Games') {
-            document.body.style.backgroundImage = `url('../public/JavaScript.png')`
+            document.body.style.backgroundImage = `url('../public/JavaScript.jpeg')`
+            APP_LIST.style = 'display:grid'
         } else if(e.target.textContent === 'Minecraft Bedrock Addons') {
             document.body.style.backgroundImage = `url('../public/minecraft.png')`
+            APP_LIST.style = 'display:none'
         } else if(e.target.textContent === 'jQuery/Ajax Applications') {
            document.body.style.backgroundImage = `url('../public/yellowImage.png')`
-           e.target.parentElement.style = 'opacity: 1'
-          
-        }
+           APP_LIST.style = 'display:none'
+        } 
 
     } else if(e.target.className === 'itemDescriptionOn'){
         e.stopPropagation()
-        elementTitle.parentElement.style = 'opacity:1'
+        elementTitle.parentElement.style = 'background-color:#f4f754'
         e.target.className = 'itemDescription'
         e.target.previousElementSibling.className = 'itemTitle'
-        document.body.style.backgroundImage = `url('../public/yellowImage.png')`
+        APP_LIST.style = 'display:none'
+        document.body.style.backgroundImage = `url('../public/BackgroundMain.jpg')`
     } 
 })
 
-document.body.addEventListener('click', function () {
-            
-        document.body.style.backgroundImage = `url('')`
-        elementTitle.parentElement.style = 'opacity:1'
-        elementTitle.className = 'itemTitle'
-        elementDes.className = 'itemDescription'
-})
+
 
 
 
