400024677cba501a2e65824bebd96ff25fee0bb9
unknown <asazycat@gmail.com>
2025-11-11 18:12:58 +0000

    kanban form

--- src/apps/javascriptApplications/kanban/kanban.js
+++ src/apps/javascriptApplications/kanban/kanban.js
@@ -1,12 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const addToBoard = document.getElementsByClassName('add');
+    const addToBoardForm = document.getElementsByClassName('contents_tobe_Added');
     const pageNumber = document.getElementById('number');
     const boardOne = document.getElementById('board1');
     const boardTwo = document.getElementById('board2');
     const boardThree = document.getElementById('board3');
     const nextButton = document.getElementById('next');
     const previousButton = document.getElementById('previous');
+
+    const div = document.createElement('div');
+    const title = document.createElement('h2');
+    const contents = document.createElement('p');
+    const formObj = {}
+
     const arr = ['1/3','2/3','3/3']
     let page = 0
+    if (window.innerWidth < 800) {
+        kanbanShow(page);
+        pageNumber.textContent = '1/3'
+    }
     
     nextButton.addEventListener ('click', (e) => {
         page++
@@ -56,19 +68,44 @@ document.addEventListener('DOMContentLoaded', () => {
     
 }
 
-window.addEventListener('resize', (e) => {
-        if(e.target.innerWidth < 800) {
-            pageNumber.textContent = '1/3'
-            kanbanShow(0)
-        } else if(e.target.innerWidth >= 800) {
-            boardOne.classList.add('kanban_show')
-            boardTwo.classList.add('kanban_show')
-            boardThree.classList.add('kanban_show')
-            boardOne.classList.remove('kanban_hide')
-            boardTwo.classList.remove('kanban_hide')
-            boardThree.classList.remove('kanban_hide')
-        }
+    window.addEventListener('resize', (e) => {
+            if(e.target.innerWidth < 800) {
+                pageNumber.textContent = '1/3'
+                kanbanShow(0)
+            } else if(e.target.innerWidth >= 800) {
+                boardOne.classList.add('kanban_show')
+                boardTwo.classList.add('kanban_show')
+                boardThree.classList.add('kanban_show')
+                boardOne.classList.remove('kanban_hide')
+                boardTwo.classList.remove('kanban_hide')
+                boardThree.classList.remove('kanban_hide')
+            }
+    });
+
+
+    Array.from(addToBoard).forEach(eachHTMLTag => {
+        eachHTMLTag.firstElementChild.addEventListener('click', (e) => {
+            e.target.parentElement.style.display = 'none'
+            e.target.parentElement.nextElementSibling.style.display = 'block'
+        
+        });
+      
+    Array.from(addToBoardForm).forEach((eachForm) => {
+        eachForm.firstElementChild.addEventListener('submit',function(e) {
+            e.stopImmediatePropagation();
+            e.preventDefault();
+            this.parentElement.previousElementSibling.style.display = 'block'
+            this.parentElement.style.display = 'none'
+            const formData = new FormData(this)
+            for (const [key,value] of formData.entries()) {
+           formObj[key] = value
+            }
+        console.log(formObj)
+        })
+
+        
     })
+})
 
 });
 
